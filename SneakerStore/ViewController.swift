ac9d25ff6427377cf32939f6dbcf9428268a3d60
Nauan Akylbayev <@n.akylbaev>
2023-12-13 18:55:49 +0600

    hello ios

---
+++ SneakerStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SneakerStore
+//
+//  Created by Admin  on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
